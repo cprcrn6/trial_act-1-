1029b54d7429191e9eccf0403ba4f8e73d3414f1
sheena-dullete <sheenadullete1226@gmail.com>
2025-04-22 01:30:14 +0800

    Initial Commit

--- Program.cs
+++ Program.cs
@@ -1,2 +1,19 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        string name1, name2;
+
+        Console.Write("Enter First Name: ");
+        name1 = Console.ReadLine();
+
+        Console.Write("Enter Second Name: ");
+        name2 = Console.ReadLine();
+
+        ShowNames(name1, name2);
+    }
+
+
+}
